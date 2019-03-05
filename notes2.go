a8644c1ea3ac906188db566ad3a376ed75d6b12a
Alex Brashear <alexbrashear@gmail.com>
2019-03-05 14:14:19 -0500

    Added some functionality to notes2.go

---
+++ notes2.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+   
+   fmt.Println("Hello World pt. 2")
+
+   /* Go Constants */
+
+      /* Constants/literals are fixed values that cannot be modified during program execution. */
+
+
+
+}
